9e66fb4cd2392bd9f7a60c5e1571d3f599148f0d
TheCoolMan13 <titust.ticusan@gmail.com>
2026-02-01 14:10:49 +0200

    Laptop Push

---
+++ 20260128/main5.c
@@ -0,0 +1,16 @@
+/*
+Să se scrie o funcție care primește două șiruri de caractere și le concatenează cu un 
+spațiu între ele, folosind un bloc de memorie alocat dinamic pentru rezultat. 
+Funcția va returna acest bloc de memorie. Să se verifice funcția cu două șiruri de 
+caractere introduse de la tastatură, fiecare de maxim 100 de caractere.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    char s1[101] ,s2[101];
+    
+    return 0;
+}
